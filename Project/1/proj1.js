2880ba73cdc3a4d06e07bd17b0aa619fec54b4c7
jasoleun <jasoleun@gmail.com>
2025-07-15 16:33:31 -0700

    Added drawPieces function

--- Project/1/proj1.js
+++ Project/1/proj1.js
@@ -11,33 +11,50 @@ cBoard[5] = ["grey", "", "grey", "", "grey", "", "grey", ""];
 cBoard[6] = ["", "grey", "", "grey", "", "grey", "", "grey"];
 cBoard[7] = ["grey", "", "grey", "", "grey", "", "grey", ""];
 
-
 function drawBoard() {
   for (let y = 0; y < cBoard.length; y++) {
-
     if (y % 2 == 0) {
       for (let i = 0; i < cBoard.length; i++) {
         if (i % 2 == 0) {
           ctx.fillStyle = "white";
-          ctx.fillRect(i * 100, y*100, 100, 100);
+          ctx.fillRect(i * 100, y * 100, 100, 100);
         } else {
           ctx.fillStyle = "black";
-          ctx.fillRect(i * 100, y*100, 100, 100);
+          ctx.fillRect(i * 100, y * 100, 100, 100);
         }
       }
     } else {
-        for (let i = 0; i < cBoard.length; i++) {
+      for (let i = 0; i < cBoard.length; i++) {
         if (i % 2 == 0) {
           ctx.fillStyle = "black";
-          ctx.fillRect(i * 100, y*100, 100, 100);
+          ctx.fillRect(i * 100, y * 100, 100, 100);
         } else {
           ctx.fillStyle = "white";
-          ctx.fillRect(i * 100, y*100, 100, 100);
+          ctx.fillRect(i * 100, y * 100, 100, 100);
         }
       }
-        
+    }
+  }
+}
+
+function drawPieces() {
+  for (let i = 0; i < cBoard.length; i++) {
+    for (let j = 0; j < cBoard[i].length; j++) {
+      if (cBoard[i][j] === "red") {
+        ctx.fillStyle = "red";
+      }
+      if (cBoard[i][j] === "grey") {
+        ctx.fillStyle = "grey";
+      }
+      if (cBoard[i][j] !== "") {
+        ctx.beginPath();
+        ctx.arc((j * 100) + 50, (i * 100) + 50, 35, 0, Math.PI * 2);
+        ctx.fill();
+      }
     }
   }
 }
 
 drawBoard();
+
+drawPieces();
