1aaafbb9256bc1eee2ea31695cf12d9a9b5df644
jasoleun <jasoleun@gmail.com>
2025-07-22 14:39:59 -0700

    nested for loop to replace all string represented pieces with objs

--- Project/2/proj2.js
+++ Project/2/proj2.js
@@ -11,7 +11,6 @@ cBoard[5] = ["grey", "", "grey", "", "grey", "", "grey", ""];
 cBoard[6] = ["", "grey", "", "grey", "", "grey", "", "grey"];
 cBoard[7] = ["grey", "", "grey", "", "grey", "", "grey", ""];
 
-
 function drawBoard() {
   for (let y = 0; y < cBoard.length; y++) {
     if (y % 2 == 0) {
@@ -40,25 +39,32 @@ function drawBoard() {
 
 drawBoard();
 
+function Piece(row, col, color) {
+  this.row = row;
+  this.col = col;
+  this.color = color;
+  this.isClicked = false;
+  this.isKing = false;
 
-
-function Piece(row,col,color){
-    this.row = row;
-    this.col = col;
-    this.color = color;
-    this.isClicked = false;
-    this.isKing = false;
-
-    this.draw = function() {
-        ctx.fillStyle = this.color;
-        ctx.beginPath();
-        ctx.arc(this.col * 100 + 50, this.row * 100 + 50, 35, 0, Math.PI * 2);
-        ctx.fill();
-    };
-
-    
+  this.draw = function () {
+    ctx.fillStyle = this.color;
+    ctx.beginPath();
+    ctx.arc(this.col * 100 + 50, this.row * 100 + 50, 35, 0, Math.PI * 2);
+    ctx.fill();
+  };
 }
 
-//for testing 
-let p = new Piece(2, 3, "red");
-p.draw();
+//for testing
+for (let i = 0; i < cBoard.length; i++) {
+  for (let j = 0; j < cBoard[i].length; j++) {
+    if (cBoard[i][j] == "red") {
+      cBoard[i][j] = new Piece(i, j, "red");
+    }
+    if (cBoard[i][j] == "grey") {
+      cBoard[i][j] = new Piece(i, j, "grey");
+    }
+    if (cBoard[i][j] != "") {
+      cBoard[i][j].draw();
+    }
+  }
+}
